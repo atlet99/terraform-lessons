4ecb3cfe5c4ceae5fe514e626f3953ffb04b2b07
atlet99 <joha.shadibekov@gmail.com>
2023-11-23 01:51:21 +0600

    Minor changes

--- Lesson-1/Lesson-1.tf
+++ Lesson-1/Lesson-1.tf
@@ -1,10 +1,13 @@
 provider "aws" {
-    access_key = "gfdgf"
-    secret_key = "fsdfdsjfhs"
-    region =  "eu-central-1"
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_KEY
+  region     = var.AWS_REGION_ID
 }
 
-resource "aws_instance" "my_Ubuntu" {
-  ami = "ami-0432nfjs432"
-  instance_type = "t3.micro"
+resource "aws_instance" "Dummy_instance" {
+  ami           = var.AWS_AMI_ID
+  instance_type = var.AWS_INSTANCE_TYPE
+  tags = {
+    Name = "server-1-test"
+  }
 }
